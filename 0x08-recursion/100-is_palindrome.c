84a97ce1964cd09f9a86869d9b1a5cb023c87e40
udobeke <pchinagwam@gmail.com>
2022-09-28 04:13:18 -0700

    we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,3 +27,13 @@ int pal_checker(char *s, int i, int j)
 	else
 		return (0);
 }
+/**
+ * is_palindrome - check if s is palindrome
+ * @s: base address for string.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	return (pal_checker(s, 0, _strlen_recursion(s) - 1));
+}
