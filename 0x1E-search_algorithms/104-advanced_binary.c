8ebe5529ef7037cb571770466773743aa0a6a828
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-13 18:35:42 +0100

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
+
+int binary_search_recursion(int *array, int value, size_t low, size_t high);
 
 /**
  * advanced_binary - searches for a value in a sorted array of integers
@@ -12,36 +13,47 @@
  *
  * Return: index where value is located or -1 if value is not present or array is NULL
  */
-int advanced_binary(int *array, size_t size, int value)
+int binary_search_recursion(int *array, int value, size_t low, size_t high)
 {
-    size_t i, half;
-    int tmp;
+	size_t mid, i;
 
-    if (array == NULL || size == 0)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (i = 0; i < size; i++)
-        printf("%d%s", array[i], i == size - 1 ? "\n" : ", ");
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+	{
+		printf("%d", array[i]);
+		if (i < high)
+			printf(", ");
+		if (i == high)
+			printf("\n");
+	}
+	mid = (low + high) / 2;
 
-    half = (size - 1) / 2;
+	if (value == array[low])
+		return (low);
 
-    if (array[half] == value)
-    {
-        if (size == 1 || array[half - 1] < value)
-            return (half);
-    }
-
-    else if (array[half] < value)
-    {
-        tmp = advanced_binary(array + half + 1, size - half - 1, value);
-        if (tmp != -1)
-            return (half + 1 + tmp);
-    }
-
-    else
-        return (advanced_binary(array, half + 1, value));
-
-    return (-1);
+	if (array[low] != array[high])
+	{
+		if (array[mid] < value)
+			return (binary_search_recursion(array, value, mid + 1, high));
+		if (array[mid] >= value)
+			return (binary_search_recursion(array, value, low, mid));
+	}
+	return (-1);
 }
 
+/**
+* Return: first index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t low = 0, high = size - 1;
+
+	if (!array)
+		return (-1);
+
+	return (binary_search_recursion(array, value, low, high));
+}
